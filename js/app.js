fb5ef39c3abf70e0998bde2a9cb566c875a15703
yeol <yeol8810@gmail.com>
2023-10-12 21:52:57 +0900

    refactoring 중간 저장

--- js/app.js
+++ js/app.js
@@ -1,149 +1,96 @@
-// button에 addEventListener 생성
-const numBtn = document.querySelectorAll(".button");
-const operatorBtn = document.querySelectorAll(".operator");
+// 전역변수
+// 첫번째 숫자 배열
+let firstNumArr = [];
+let secondNumArr = [];
+let operator = "";
 
-// 결과 span
-const resultSpan = document.querySelector(".result");
+// 숫자버튼 button event
+const numBtns = document.querySelectorAll(".button");
+numBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const {
+      currentTarget: { value },
+    } = event;
 
-// 현재 상태 type
-const currentType = document.querySelector("#current_type");
-
-// 연산자 저장
-let currentOperator = "";
-let waitingOperator = "";
-
-// 대기 값, 결과 값
-let currentNumber = "0";
-let resultNumber = "0";
-
-//숫자 버튼 클릭 '.' 포함
-numBtn.forEach((item) => {
-  item.addEventListener("click", (event) => {
-    if (currentType.value !== "0") {
-      currentType.value = "0";
-      printResult("");
+    // 점이 없을때
+    if (!firstNumArr.includes(".") || !secondNumArr.includes(".")) {
+      if ((firstNumArr[0] === "0" || secondNumArr[0] === "0") && value === "0")
+        return;
     }
 
-    // 현재 숫자
-    currentNumber = String(currentNumber) + event.target.innerText;
-    if (!currentNumber.includes(".")) {
-      if (currentNumber.charAt("0") === "0")
-        currentNumber = currentNumber.substring(1);
-    }
-    printResult(currentNumber);
+    // 0다음 dot 이 오지 않는다면.. 0을 삭제....
+    // 연산자가 입력되었는지 확인 후 배열에 나눠서 숫자 받기
+    operator === ""
+      ? pushNum(firstNumArr, value)
+      : pushNum(secondNumArr, value);
   });
 });
 
-//연산자 버튼 클릭
-operatorBtn.forEach((item) => {
-  item.addEventListener("click", (event) => {
-    waitingOperator = currentOperator;
-    currentOperator = event.target.innerText;
+// 연산자 버튼 button event
+const operatorBtns = document.querySelectorAll(".operator");
+operatorBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const {
+      currentTarget: { value },
+    } = event;
 
-    // currentOperator가 clear 이면 모든걸 초기화한다.
-    if (currentOperator === "clear") {
-      calculator(currentOperator, resultNumber, currentNumber);
+    // 입력 받은 연산자가 delete일 때
+    if (value === "delete") {
+      if (operator === "") popNum(firstNumArr);
+      else popNum(secondNumArr);
+      return;
+    }
+    // 입력 받은 연산자가 clear일때
+    if (value === "clear") {
+      firstNumArr = [];
+      secondNumArr = [];
+      operator = "";
+      pushNum(firstNumArr, "0");
       return;
     }
 
-    // currentOperator가 = 이면 waitingOperator에 있는 연산을 하고 waitingOperator를 초기
-    if (currentOperator === "=") {
-      calculator(waitingOperator, resultNumber, currentNumber);
-      waitingOperator = "";
-      return;
-    }
-
-    if (waitingOperator === "") {
-      resultNumber = currentNumber;
-      printResult(resultNumber);
-      currentNumber = "0";
-      currentType.value = "1";
-    } else {
-      calculator(waitingOperator, resultNumber, currentNumber);
-    }
+    operator = value;
   });
 });
 
-// 결과 출력 함수
-const printResult = (num) => {
-  const option = {
-    maximumFractionDigits: 6,
-  };
-  num = num === "" ? "0" : num;
-  resultSpan.innerText = num
-    .toString()
+// 결과 및 입력 받은 숫자 출력 span
+const result = document.querySelector("#result");
+
+// 입력받은 숫자 출력 함수
+const printNum = (numArr) => {
+  result.innerText = numArr
+    .join("")
     .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 };
 
-// currentNum, type 초기화
-const resetCurrentState = () => {
-  currentType.value = "1";
-  currentNumber = 0;
+/**
+ * 입력 받은 숫자 배열에 push 함수
+ * @param numArr
+ * @param num
+ */
+const pushNum = (numArr, num) => {
+  numArr.push(num);
+  printNum(numArr);
 };
 
-const delDot = (num) => {
-  if (num.length > 0) {
-    if (num.charAt(num.length - 1) === ".")
-      return num.substring(0, num.length - 1);
-    else return num;
-  } else {
-    return "0";
-  }
+/**
+ * 숫자 삭제 pop 함수
+ * @param numArr
+ */
+const popNum = (numArr) => {
+  numArr.pop();
+  printNum(numArr);
 };
 
-const calculator = (operator, operatorNum1, operatorNum2) => {
-  operatorNum1 = delDot(operatorNum1);
-  operatorNum2 = delDot(operatorNum2);
-
-  switch (operator) {
-    case "+":
-      resultNumber = String(
-        parseFloat(operatorNum1) + parseFloat(operatorNum2)
-      );
-      printResult(resultNumber);
-      resetCurrentState();
-      break;
-    case "-":
-      resultNumber = String(
-        parseFloat(operatorNum1) - parseFloat(operatorNum2)
-      );
-      printResult(resultNumber);
-      resetCurrentState();
-      break;
-    case "X":
-      resultNumber = String(
-        parseFloat(operatorNum1) * parseFloat(operatorNum2)
-      );
-      printResult(resultNumber);
-      resetCurrentState();
-      break;
-    case "/":
-      resultNumber = String(
-        parseFloat(operatorNum1) / parseFloat(operatorNum2)
-      );
-      printResult(resultNumber);
-      resetCurrentState();
-      break;
-    case "=":
-      printResult(resultNumber);
-      break;
-    case ".":
-      currentNumber = operatorNum2 + ".";
-      printResult(currentNumber);
-      break;
-    case "clear":
-      resultNumber = "0";
-      currentNumber = "0";
-      currentOperator = "";
-      waitingOperator = "";
-      currentType.value = "0";
-      printResult(0);
-      break;
-    default:
-      resultNumber = operatorNum2;
-      printResult(resultNumber);
-      resetCurrentState();
-
-      break;
-  }
+const removeZero = (numArr) => {
+  numArr.shift();
+  printNum(numArr);
 };
+
+const Person = (name, age) => {
+  (this.name = name), (this.age = age);
+};
+
+const person1 = new Person();
