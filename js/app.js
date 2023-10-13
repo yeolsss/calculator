9312ddfe31d4dca82d8f52f5d5cc0b88ab21545d
yeol <yeol8810@gmail.com>
2023-10-13 09:47:26 +0900

    refactoring - 항상 입력 받을 배열 currentNumArr 추가 - prevNumArr는 연산자가 입력되면 currentNumArr의 값을 할당 하는 배열 - resultNumArr는 항상 결과값을 갖는 배열

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 // 전역변수
-// 첫번째 숫자 배열
-const prevNumArr = [];
-const resultNumArr = [];
+// 항상 입력받는 배열
+let currentNumArr = ["0"];
+// 연산자가 입력되면 nextNumArr에 값을 받아오는 배열
+let prevNumArr = ["0"];
+// 항상 결과값만 갖는 배열
+let resultNumArr = ["0"];
+// 연산자를 받는 변수
 let operator = "";
 
 // 숫자버튼 button event
@@ -14,19 +18,19 @@ numBtns.forEach((btn) => {
     } = event;
 
     // 배열에 이미 '.'이 있고 value가 '.' 이면 아무 행동을하지 않고 종료.
-    if (resultNumArr.includes(".") && value === ".") {
+    if (currentNumArr.includes(".") && value === ".") {
       return;
     }
 
     // 배열의 0번 index에 '0'이 있을때
-    if (resultNumArr[0] === "0") {
+    if (currentNumArr[0] === "0") {
       // 배열에 '.'이 없고 입력값이 '.'이 아닐때
-      if (!resultNumArr.includes(".") && value !== ".") {
-        removeZero(resultNumArr);
+      if (!currentNumArr.includes(".") && value !== ".") {
+        removeZero(currentNumArr);
       }
     }
 
-    pushNum(resultNumArr, value);
+    pushNum(currentNumArr, value);
   });
 });
 
@@ -46,17 +50,35 @@ operatorBtns.forEach((btn) => {
     }
     // 입력 받은 연산자가 clear일때
     if (value === "clear") {
-      removeArrElement(prevNumArr);
-      removeArrElement(resultNumArr);
+      prevNumArr = ["0"];
+      currentNumArr = ["0"];
+      resultNumArr = ["0"];
+      printNum(resultNumArr);
       operator = "";
-      pushNum(resultNumArr, "0");
       return;
     }
 
     // 연산자 버튼이 'delete', 'clear'가 아닌 value가 들어왔을때
     // resultNumArr를 prevNumArr로 옮긴 후 resultNumArr 초기화
+    // = 연산자가 입력되면 계산 시작 그게 아닐경우
+    // resultNumArr의 요소를 prevNumArr로 할당 후 resultNumArr 초기화
+    if (operator !== "") {
+      const result = calculation();
+      resultNumArr = [...(result + "")];
+      prevNumArr = [...resultNumArr];
+      currentNumArr = ["0"];
+      if (value === "=") {
+        printNum(resultNumArr);
+        return;
+      }
+    } else {
+      prevNumArr = [...currentNumArr];
+      resultNumArr = [...currentNumArr];
+      currentNumArr = ["0"];
+    }
 
-    operator = value;
+    printNum(resultNumArr);
+    operator = value !== "=" ? value : "";
   });
 });
 
@@ -91,14 +113,6 @@ const popNum = (numArr) => {
 };
 
 /**
- * 배열 요소 삭제 함수
- * @param numArr
- */
-const removeArrElement = (numArr) => {
-  numArr.splice(0, numArr.length);
-};
-
-/**
  * 배열 제일 앞의 요소가 '0'일 때 삭제하는 함수
  * @param numArr
  */
@@ -107,4 +121,29 @@ const removeZero = (numArr) => {
   printNum(numArr);
 };
 
-const calculation = () => {};
+const calculation = () => {
+  switch (operator) {
+    case "+":
+      return (
+        parseFloat(prevNumArr.join("")) + parseFloat(currentNumArr.join(""))
+      );
+    case "-":
+      return (
+        parseFloat(prevNumArr.join("")) - parseFloat(currentNumArr.join(""))
+      );
+    case "X":
+      return (
+        parseFloat(prevNumArr.join("") === "0" ? "1" : prevNumArr.join("")) *
+        parseFloat(
+          currentNumArr.join("") === "0" ? "1" : currentNumArr.join("")
+        )
+      );
+    case "/":
+      return (
+        parseFloat(prevNumArr.join("") === "0" ? "1" : prevNumArr.join("")) /
+        parseFloat(
+          currentNumArr.join("") === "0" ? "1" : currentNumArr.join("")
+        )
+      );
+  }
+};
